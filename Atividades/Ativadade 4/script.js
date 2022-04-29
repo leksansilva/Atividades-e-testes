b659c7c167248eaa43c2f481e4e478e76e980b7d
leksansilva <69767375+leksansilva@users.noreply.github.com>
2022-04-29 08:40:26 -0300

    Modificações

--- Atividades/Ativadade 4/script.js
+++ Atividades/Ativadade 4/script.js
@@ -1,7 +1,7 @@
 const onSubmit = (event) => {
   event.preventDefault();
 
-  const meses = [
+  const mounth = [
     "janeiro",
     "fevereiro",
     "março",
@@ -15,34 +15,53 @@ const onSubmit = (event) => {
     "novembro",
     "dezembro",
   ];
-  const data = new FormData(event.currentTarget);
-  const name = data.get("name");
-  const email = data.get("email");
-  const city = data.get("city");
-  const news = data.get("news");
-  const question = data.get("question");
-  const count = parseInt(question);
 
+  const privacy = {
+    accept:
+      "Que bom que você leu e concorda com nossa política de privacidade. Seu formulário SERÁ submetido para cadastro",
+    deny: "É uma pena você não concordar com nossa política de privacidade. INFELIZMENTE  seu formulário NÃO SERÁ  submetido para cadastro",
+  };
+  const data = new FormData(event.currentTarget);
+  const setValue = (value) => value;
+  var values;
+  data.forEach((value, name) => {
+    values = setValue({ ...values, [name]: value });
+  });
+  console.log(values);
+  const count = parseInt(values.question);
   let i,
     x = 0,
     z = 1;
-  for (i = 0; i < count; i++) {
-    if (x == 0) {
-      document.getElementById("plano").innerHTML += "<br> Ano " + z + "<br>";
-    }
 
-    if (x < 12) {
-      document.getElementById("plano").innerHTML += meses[x] + ", ";
-    }
-    x++;
-    if (x == 12) {
-      x = 0;
-      z++;
+  document.getElementById("plano").innerHTML = "";
+  if (count == 0) {
+    document.getElementById("plano").innerHTML = "O tempo não foi informado!";
+  } else {
+    for (i = 0; i < count; i++) {
+      if (x == 0) {
+        document.getElementById("plano").innerHTML += "<br> Ano " + z + "<br>";
+      }
+
+      if (x < 12) {
+        document.getElementById("plano").innerHTML += mounth[x] + ", ";
+      }
+      x++;
+      if (x == 12) {
+        x = 0;
+        z++;
+      }
     }
   }
-  document.getElementById("nameOk").innerHTML = name;
-  document.getElementById("emailOk").innerHTML = email;
-  document.getElementById("cityOk").innerHTML = city;
+
+  document.getElementById("nameOk").innerHTML = values.name;
+  document.getElementById("emailOk").innerHTML = values.email;
+  document.getElementById("cityOk").innerHTML = values.city;
   document.getElementById("left").style = "display:none";
   document.getElementById("form-submit").style = "display:block";
+  document.getElementById("optionOk").innerHTML = values.news
+    ? " RECEBER "
+    : " NÃO RECEBER ";
+  document.getElementById("privacyOk").innerHTML = values.form_accept
+    ? privacy.accept
+    : privacy.deny;
 };
---
+++ Atividades/Ativadade 5/script.js
@@ -0,0 +1,107 @@
+//var dataTable = [];
+var id = 1;
+const setData = (value) => value;
+const onSubmit = (ev) => {
+  ev.preventDefault();
+  const data = new FormData(ev.currentTarget);
+
+  var values = setData({ id });
+  data.forEach((value, name) => {
+    values = setData({ ...values, [name]: value });
+  });
+  //dataTable.push(values);
+  id++;
+  showData(values);
+  reset();
+};
+
+const showData = (values) => {
+  const valuesArray = Object.entries(values);
+  document.getElementById("tbody").innerHTML += `
+    <tr id="tr${values.id}">
+    ${valuesArray.map((value) => `<td>${value[1]}</td>`).join("")}
+      <td>
+        <button 
+          class='btn btn-option' 
+          onclick='handleEdit(${values.id}, ${JSON.stringify(values)})'>
+          Editar 
+        </button>
+        <button 
+          class='btn btn-option' 
+          onclick='handleDelete(${values.id},tr${values.id})'>
+          Apagar
+        </button>
+      </td>
+    </tr>
+    `;
+};
+
+const handleEdit = (id, values) => {
+  const ID = parseInt(id);
+  toggleButton(ID);
+  // const values = dataTable.find((value) => value.id === ID);
+  setOldValues(values);
+};
+
+const handleDelete = (id, tr) => {
+  const ID = parseInt(id);
+  tr.remove();
+  //dataTable = dataTable.filter((value) => value.id !== ID);
+};
+
+const setOldValues = (values) => {
+  const valuesArray = Object.entries(values);
+  valuesArray.forEach((value) => {
+    if (value[0] !== "id") document.getElementById(value[0]).value = value[1];
+  });
+};
+
+const setNewValues = (oldId, tr) => {
+  const form = document.getElementById("form");
+  var newValues = setData({ id: oldId });
+  for (const i of form.children) {
+    if (i.id) newValues = setData({ ...newValues, [i.name]: i.value });
+  }
+  /* dataTable  = dataTable.map((value) =>
+    value.id === id ? (value = newValues) : value
+  ); */
+  const valuesArray = Object.entries(newValues);
+
+  tr.innerHTML = `
+  ${valuesArray.map((value) => `<td>${value[1]}</td>`).join("")}
+  <td>
+    <button class='btn btn-option' onclick='handleEdit(${
+      newValues.id
+    }, ${JSON.stringify(newValues)})'>Editar </button>
+    <button class='btn btn-option' onclick='handleDelete(${newValues.id},tr${
+    newValues.id
+  })'>Apagar</button>
+  </td>`;
+  toggleButton();
+  reset();
+};
+
+const reset = () => {
+  document.getElementById("reset").click();
+  document.getElementById("name").focus();
+};
+
+const handleReset = () => {
+  toggleButton();
+  reset();
+};
+
+const toggleButton = (id) => {
+  const button = document.getElementById("submit");
+  if (id) {
+    button.type = "button";
+    button.style = " background-color: #407094;";
+    button.innerHTML = "Editar";
+    button.setAttribute("onclick", `setNewValues(${id}, tr${id})`);
+  } else {
+    button.type = "submit";
+    button.style = "background-color: #31b454;";
+    button.innerHTML = "Salvar";
+    button.removeAttribute("onclick");
+  }
+};
